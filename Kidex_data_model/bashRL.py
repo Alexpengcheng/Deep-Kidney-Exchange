ae9aec517f72d87ea45c0430f7291fb4d9fd99de
pengcheng_XU <alexpengcheng@gmail.com>
2017-10-12 00:41:36 -0400

    Add files via upload

---
+++ Kidex_data_model/bashRL.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import os
+
+'''
+Automatically distribute the RL experiment
+'''
+
+M: [32, 64, 128, 256]
+K: [32, 128, 512]
+CYCLE_CAP: [2, 3, 4]
+CHAIN_CAP: [0, 3, 6, 9]
+SAMPLE = 5
+CHAINSAMPLE = 3
+CYCLESAMPLE = 3
+TAU = 5
+ALPHA = 0.05
+
+
+for m in M:
+    for k in K:
+        for cycle in CYCLE_CAP:
+            for chain in CHAIN_CAP:
+                os.system('nohup python3 kidex_trpo.py '
+                        '--cyclecap %s '
+                        '--chaincap %s '
+                        '--M %s '
+                        '--K %s '
+                        '--sample %s '
+                        '--chainsample %s '
+                        '--cyclesampl %s '
+                        '--tau %s '
+                        '--alpha %s '
+                          '>M%sK%sChain%sCycle%s &' % (cycle, chain, m, k, SAMPLE, CHAINSAMPLE, CYCLESAMPLE, TAU, ALPHA,
+                                                       m,k,chain,cycle))
